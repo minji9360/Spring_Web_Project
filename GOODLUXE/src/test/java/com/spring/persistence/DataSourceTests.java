b2ce804e72b1786aafd719db52a6796196d66a24
jtbc <jtbc@naver.com>
2020-04-25 00:06:48 +0900

    mybatis 설정 완료

--- GOODLUXE/src/test/java/com/spring/persistence/DataSourceTests.java
+++ GOODLUXE/src/test/java/com/spring/persistence/DataSourceTests.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,10 +24,17 @@ public class DataSourceTests {
 
 	@Setter(onMethod_ = { @Autowired })
 	private DataSource dataSource;
+	
+	@Setter(onMethod_ = { @Autowired })
+	private SqlSessionFactory sqlSessionFactory;
+	
 	@Test
-	public void testConnection() {
+	public void testMyBatis() {
 		
-		try (Connection con = dataSource.getConnection()) {
+		try (SqlSession session = sqlSessionFactory.openSession();
+				Connection con = session.getConnection();
+			) {
+			log.info(session);
 			log.info(con);
 		} catch(Exception e) {
 			fail(e.getMessage());
