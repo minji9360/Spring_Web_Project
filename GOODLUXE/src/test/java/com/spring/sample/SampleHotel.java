0cc65174507c7c21b04575847ce6d0db10b5c44a
jtbc <jtbc@naver.com>
2020-04-24 11:15:28 +0900

    JDBC 연결 및 테스트 완료

---
+++ GOODLUXE/src/test/java/com/spring/sample/SampleHotel.java
@@ -0,0 +1,16 @@
+package com.spring.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Component
+@ToString
+@Getter
+@AllArgsConstructor
+public class SampleHotel {
+
+	private Chef chef;
+}
