b86aecfb2a877fa68bd6524c196a094b2ad0228e
kmj9360 <minji4853@naver.com>
2020-05-04 22:39:58 +0900

    영속/비즈니스 계층 CRUD 구현 예제 완료

--- GOODLUXE/src/main/java/com/spring/mapper/BoardMapper.java
+++ GOODLUXE/src/main/java/com/spring/mapper/BoardMapper.java
@@ -2,12 +2,20 @@ package com.spring.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Select;
-
 import com.spring.domain.BoardVO;
 
 public interface BoardMapper {
 
-	@Select("select * from tbl_board where bno > 0")
+	//@Select("select * from tbl_board where bno > 0") // BoardMapper.xml���� SQL���� ó���Ǿ����� ����
 	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
 }
