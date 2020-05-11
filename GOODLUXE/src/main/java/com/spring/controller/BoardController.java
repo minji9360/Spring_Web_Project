4d168e069160269070238683febc0d92ae8f24c2
kmj9360 <minji4853@naver.com>
2020-05-12 04:40:47 +0900

    검색 처리 예제 완료 _ UriCompnentsBuilder 이용
    - Criteria 클래스에서 queryParam() 메서드 이용해 필요한 파라미터를 손쉽게 추가할 수 있음
    - getListLink()를 통해 Get 방식에 적합한 URL 인코딩 결과를 만들어줌
      한글 처리에 신경쓰지 않아도 됨
    - 이렇게 생성된 URL은 화면에서도 유용하게 사용됨
       JavaScript 사용할 수 없는 상황에서 링크 처리해야 할 때 사용

--- GOODLUXE/src/main/java/com/spring/controller/BoardController.java
+++ GOODLUXE/src/main/java/com/spring/controller/BoardController.java
@@ -65,33 +65,23 @@ public class BoardController {
 	}
 	
 	@PostMapping("/modify")
-	public String modify(BoardVO board, @ModelAttribute("cri") Criteria cri, RedirectAttributes rttr) {
+	public String modify(BoardVO board, Criteria cri, RedirectAttributes rttr) {
 		log.info("modify: " + board);
 		
 		if(service.modify(board)) {
 			rttr.addFlashAttribute("result", "success");
 		}
 		
-		rttr.addAttribute("pageNum", cri.getPageNum());
-		rttr.addAttribute("amount", cri.getAmount());
-		rttr.addAttribute("type", cri.getType());
-		rttr.addAttribute("keyword", cri.getKeyword());
-		
-		return "redirect:/board/list";
+		return "redirect:/board/list" + cri.getListLink();
 	}
 	
 	@PostMapping("/remove")
-	public String remove(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri, RedirectAttributes rttr) {
+	public String remove(@RequestParam("bno") Long bno, Criteria cri, RedirectAttributes rttr) {
 		log.info("remove.........." + bno);
 		if(service.remove(bno)) {
 			rttr.addFlashAttribute("result", "success");
 		}
-
-		rttr.addAttribute("pageNum", cri.getPageNum());
-		rttr.addAttribute("amount", cri.getAmount());
-		rttr.addAttribute("type", cri.getType());
-		rttr.addAttribute("keyword", cri.getKeyword());
 		
-		return "redirect:/board/list";
+		return "redirect:/board/list" + cri.getListLink();
 	}
 }
