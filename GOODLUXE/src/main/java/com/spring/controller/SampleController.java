e5ac45b5036888878f4126a7fe3d29bf39bae057
kmj9360 <minji4853@naver.com>
2020-05-15 21:49:17 +0900

    @RestController 반환 타입 _ 객체

--- GOODLUXE/src/main/java/com/spring/controller/SampleController.java
+++ GOODLUXE/src/main/java/com/spring/controller/SampleController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.domain.SampleVO;
+
 import lombok.extern.log4j.Log4j;
 
 @RestController
@@ -17,5 +19,12 @@ public class SampleController {
 		log.info("MIME TYPE: " + MediaType.TEXT_PLAIN_VALUE);
 		
 		return "안녕하세요";
+	}
+	
+	@GetMapping(value = "/getSample", 
+			produces = {MediaType.APPLICATION_JSON_UTF8_VALUE, 
+						MediaType.APPLICATION_XML_VALUE })
+	public SampleVO getSample() {
+		return new SampleVO(112, "스타", "로드");
 	}
 }
