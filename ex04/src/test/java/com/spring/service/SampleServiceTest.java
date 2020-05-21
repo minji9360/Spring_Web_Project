68d157baf3f7c3bb8e35668b8ffe9a020d2897b0
kmj9360 <minji4853@naver.com>
2020-05-21 23:38:42 +0900

    AOP 테스트 예제
    - testClass() : AOP 설정을 한 Target에 대해 Proxy 객체가 정상적으로 만들어져 있는지 확인
    - testAdd() : 위의 테스트 코드를 더 세밀하게 파악하기 위해 작성한 코드
                        (testClass()는 toString()의 결과이기 때문에..)
    - logBeforeWithParam(String str1, String str2) : args를 이용한 파라미터 추적
      (여기에서 @Before() 안의 execution 표현식에서 doAdd(String, String)) && args(str1, str2)로 이전과 다르게 코드를 작성하게 되는데, Pointcut 설정에 doAdd() 메서드와 파라미터 타입을 명시하고 && args(..에 변수명을 지정하면, 이 두 가지 종류의 정보를 이용해 logBeforeWithParam() 메서드의 파라미터를 설정하게 됨.
      기존 테스트 코드를 실행하면 단순한 로그와 전달된 파라미터를 파악할 수 있음
      ( * &&args 이용한 설정은 간단히 파라미터 찾아 기록할 때는 유용하나 파라미터가 다른 여러 종류의 메서드에 적용하는 것은 간단하지 않은 단점이 있음. 이건 @Around와 ProceedingJoinPoint 이용해 해결할 것)
    
    - @AfterThrowing : 지정된 대상이 예외를 발생한 후 동작. 문제를 찾을 수 있도록 도와줌
    
    - @Around : 직접 대상 메서드를 실행할 수 있는 권한 가짐. 메서드 실행 전과 실행 후에 처리 가능
      ProceedingJoinPoint : @Around와 결합해 파라미터나 예외 등을 처리할 수 있음
      * @Before 등과 달리 @Around 적용되는 메서드의 경우, 리턴 타입은 void 아닌 타입으로 설정하고 실행 결과도 직접 반환하는 형태로 작성해야 함

---
+++ ex04/src/test/java/com/spring/service/SampleServiceTest.java
@@ -0,0 +1,32 @@
+package com.spring.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Log4j
+@ContextConfiguration({ "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
+public class SampleServiceTest {
+	
+	@Setter(onMethod_ = @Autowired)
+	private SampleService service;
+	
+	@Test
+	public void testClass() {
+		log.info(service);
+		log.info(service.getClass().getName());
+	}
+	
+	@Test
+	public void testAdd() throws Exception {
+		log.info(service.doAdd("123", "456"));
+		// log.info(service.doAdd("123", "AAA")); // Exception 처리 테스트 위한 오류 코드 ("AAA")
+	}
+
+}
