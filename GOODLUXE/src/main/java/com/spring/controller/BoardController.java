9dcefa82321dfadb68bfe050a4151e14fc9e6b85
kmj9360 <minji4853@naver.com>
2020-05-24 23:44:15 +0900

    문제 : 한글 깨짐 register: 에 데이터가 안들어감

--- GOODLUXE/src/main/java/com/spring/controller/BoardController.java
+++ GOODLUXE/src/main/java/com/spring/controller/BoardController.java
@@ -45,9 +45,17 @@ public class BoardController {
 	
 	@PostMapping("/register")
 	public String register(BoardVO board, RedirectAttributes rttr) {
+		log.info("===================================");
 		log.info("register: " + board);
-		service.register(board);
-		rttr.addFlashAttribute("result", board.getBno());
+		
+		if(board.getAttachList() != null) {
+			board.getAttachList().forEach(attach -> log.info(attach));
+		}
+
+		log.info("===================================");
+		
+		// service.register(board);
+		// rttr.addFlashAttribute("result", board.getBno());
 		
 		return "redirect:/board/list";
 	}
